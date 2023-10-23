84531537f36994fb327116e78c8c69b9adcd006f
alexandre.tulodziecki <alexandre.tulodziecki@yunextraffic.com>
2023-10-23 14:49:17 +0100

    Added code to demonstrate how to index a list..

--- list_tasks.py
+++ list_tasks.py
@@ -9,5 +9,16 @@ def run_task1():
     list = directions()
     print(list)
 
+def run_task2():
+    print("Moving...")
+    path = movements()
+    print(f"{path[0]} for {path[1]} steps")
+    print(f"{path[2]} for {path[3]} steps")
+    print(f"{path[4]} for {path[5]} steps")
+    print(f"{path[6]} for {path[7]} steps")
+def movements():
+    path = ["Move Forward", 10 , "Move Backward" , 5 ,"Move Left", 3 , "Move Right", 1]
+    return path
+
 if __name__ == "__main__":
-    run_task1()
+    run_task2()
